ad98661483c0a3a4073cb995e3478ffab301f379
egon-isnoise <egonisnoise@proton.me>
2023-03-15 17:14:11 +0100

    fixed responsiveness on phones

--- src/homeComponents/team/team.jsx
+++ src/homeComponents/team/team.jsx
@@ -11,21 +11,6 @@ const Team = () => {
     return(
         <section id="team">
             <div className="container team__container">
-            <div className="member_container">
-                <img src={IMAGE2} alt="Xefler"/>
-                    <div className="description_socials">
-                        <h1> Design </h1>
-                        <p>Xefler</p>
-                        <div className="links_socials">
-                            <a href="https://www.instagram.com/xefler_/?theme=dark" target="_blank" rel="noreferrer">
-                                <FaInstagramSquare/>
-                            </a>
-                            <a href="https://twitter.com/Xefler2022" target="_blank" rel="noreferrer">
-                                <FaTwitterSquare/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
                 
                 <div className="member_container">
                     <img src={IMAGE1} alt="egonisnoise"/>
@@ -37,6 +22,22 @@ const Team = () => {
                                 <FaInstagramSquare/>
                             </a>
                             <a href="https://twitter.com/egonisnoise" target="_blank" rel="noreferrer">
+                                <FaTwitterSquare/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="member_container">
+                <img src={IMAGE2} alt="Xefler"/>
+                    <div className="description_socials">
+                        <h1> Design </h1>
+                        <p>Xefler</p>
+                        <div className="links_socials">
+                            <a href="https://www.instagram.com/xefler_/?theme=dark" target="_blank" rel="noreferrer">
+                                <FaInstagramSquare/>
+                            </a>
+                            <a href="https://twitter.com/Xefler2022" target="_blank" rel="noreferrer">
                                 <FaTwitterSquare/>
                             </a>
                         </div>
